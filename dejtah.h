dbe2ae70dc3ab559369befeeea47082f91184322
Jan Nemec <jnemec@acrobits.cz>
2013-11-19 14:53:04 +0100

    kosmetika

--- dejtah.h
+++ dejtah.h
@@ -7,11 +7,12 @@ knihovnu zahajeni*/
 extern int MamKnihovnu;
 
 /**************************************************************************/
-/*  Dejtah - Myslici algoritmus (jeho volani)
-/* cas je v ms
+/*  Dejtah - Myslici algoritmus (jeho volani)                             */
+/* cas je v ms                                                            */
 /* PrijmiRemis muze byt NULL. Jinak tam pha da, zda by prijal remizu      */
-/* VzdejSe muze by NULL. Jinak, zda se pha vzdava			  */
-/***************************************************************************/
-cfunkce int DejTah(TUloha *uloha, TTah1 *t1, u32 cas, int maxHloubka, int *PrijmiRemis,	int *VzdejSe);
+/* VzdejSe muze by NULL. Jinak, zda se pha vzdava			              */
+/* vraci 1, pokud existuje tah, jinak 0                                   */ 
+/**************************************************************************/
+cfunkce int DejTah(TUloha *uloha, TTah1 *t1, u32 cas, int maxHloubka, int *PrijmiRemis, int *VzdejSe);
 
 #endif
