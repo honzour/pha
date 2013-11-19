9aa906e0369827d167c2b0c79063fb02f030cc52
Jan Nemec <jnemec@acrobits.cz>
2013-11-19 14:53:19 +0100

    pokracuji testy

--- testy.c
+++ testy.c
@@ -1,6 +1,7 @@
 #include "volby.h"
 #include "cotyvar.h"
 #include "partie.h"
+#include "dejtah.h"
 
 void test(TUloha *uloha, TPozice *pozice, u16* tahy, int pocet, int *slozitost)
 {
@@ -8,7 +9,10 @@ void test(TUloha *uloha, TPozice *pozice, u16* tahy, int pocet, int *slozitost)
 	*slozitost = 0;
 	for (iterace = 0; iterace < pocet; iterace++)
 	{
+		TTah1 t1;
+		
 		InitPartie(&(uloha->prt), pozice + iterace);
 		PromazTabulky(uloha);
+		DejTah(uloha, &t1, 1000 * 60 * 60 * 24, MaxHloubkaPropoctu, NULL, NULL);
 	}
 }
